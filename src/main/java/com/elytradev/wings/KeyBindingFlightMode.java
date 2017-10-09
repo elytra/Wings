96484356a0cc9be9bd3580d4feedd1557c8b04c3
Aesen Vismea <aesen@unascribed.com>
2017-10-09 03:34:50 -0400

    Add keybinds, and hacks to work around "universal" vanilla keybinds

---
+++ src/main/java/com/elytradev/wings/KeyBindingFlightMode.java
@@ -0,0 +1,39 @@
+package com.elytradev.wings;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.settings.IKeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+
+public class KeyBindingFlightMode extends KeyBinding {
+
+	public KeyBindingFlightMode(String description, IKeyConflictContext keyConflictContext, int keyCode, String category) {
+		super(description, keyConflictContext, keyCode, category);
+	}
+
+	public KeyBindingFlightMode(String description, IKeyConflictContext keyConflictContext, KeyModifier keyModifier, int keyCode, String category) {
+		super(description, keyConflictContext, keyModifier, keyCode, category);
+	}
+
+	public KeyBindingFlightMode(String description, int keyCode, String category) {
+		super(description, keyCode, category);
+	}
+	
+	@Override
+	public boolean conflicts(KeyBinding other) {
+		// Just check our context, not the other
+		if (getKeyConflictContext().conflicts(other.getKeyConflictContext())) {
+			KeyModifier keyModifier = getKeyModifier();
+			KeyModifier otherKeyModifier = other.getKeyModifier();
+			if (keyModifier.matches(other.getKeyCode()) || otherKeyModifier.matches(getKeyCode())) {
+				return true;
+			} else if (getKeyCode() == other.getKeyCode()) {
+				// Don't check all the strange ingame exceptions, we'll never use
+				// that category (and ingame->flightmode conflicts can't exist)
+				return keyModifier == otherKeyModifier;
+			}
+		}
+		return false;
+	}
+
+	
+}
---
+++ src/main/java/com/elytradev/wings/KeyEntryFlightMode.java
@@ -0,0 +1,69 @@
+package com.elytradev.wings;
+
+import com.elytradev.concrete.reflect.accessor.Accessor;
+import com.elytradev.concrete.reflect.accessor.Accessors;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiKeyBindingList;
+import net.minecraft.client.gui.GuiKeyBindingList.KeyEntry;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.text.TextFormatting;
+
+public class KeyEntryFlightMode extends KeyEntry {
+
+	private static final Accessor<GuiButton> btnReset = Accessors.findField(KeyEntry.class, "field_148281_e", "btnReset");
+	private static final Accessor<GuiButton> btnChangeKeyBinding = Accessors.findField(KeyEntry.class, "field_148280_d", "btnChangeKeyBinding");
+	private static final Accessor<String> keyDesc = Accessors.findField(KeyEntry.class, "field_148283_c", "keyDesc");
+	
+	private static final Accessor<GuiControls> controlsScreen = Accessors.findField(GuiKeyBindingList.class, "field_148191_k", "controlsScreen");
+	private static final Accessor<Integer> maxListLabelWidth = Accessors.findField(GuiKeyBindingList.class, "field_148188_n", "maxListLabelWidth");
+	
+	private final GuiKeyBindingList li;
+	private final KeyBindingFlightMode keybinding;
+	
+	public KeyEntryFlightMode(GuiKeyBindingList li, KeyBindingFlightMode keybinding) {
+		li.super(keybinding);
+		this.li = li;
+		this.keybinding = keybinding;
+	}
+	
+	@Override
+	public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, int mouseX, int mouseY, boolean isSelected, float partialTicks) {
+		Minecraft mc = Minecraft.getMinecraft();
+		
+		boolean rebinding = controlsScreen.get(li).buttonId == this.keybinding;
+		mc.fontRenderer.drawString(keyDesc.get(this), x + 90 - maxListLabelWidth.get(li), y + slotHeight / 2 - mc.fontRenderer.FONT_HEIGHT / 2, -1);
+		
+		btnReset.get(this).x = x + 210;
+		btnReset.get(this).y = y;
+		btnReset.get(this).enabled = !this.keybinding.isSetToDefaultValue();
+		btnReset.get(this).drawButton(mc, mouseX, mouseY, partialTicks);
+		
+		btnChangeKeyBinding.get(this).x = x + 105;
+		btnChangeKeyBinding.get(this).y = y;
+		btnChangeKeyBinding.get(this).displayString = this.keybinding.getDisplayName();
+		
+		boolean conflict = false;
+		boolean modifierConflict = true; // less severe form of conflict, like SHIFT conflicting with SHIFT+G
+		if (this.keybinding.getKeyCode() != 0) {
+			for (KeyBinding kb : mc.gameSettings.keyBindings) {
+				// Here's the change: keybinding.conflicts instead of kb.conflicts
+				if (kb != this.keybinding && keybinding.conflicts(kb)) {
+					conflict = true;
+					modifierConflict &= kb.hasKeyCodeModifierConflict(this.keybinding);
+				}
+			}
+		}
+
+		if (rebinding) {
+			btnChangeKeyBinding.get(this).displayString = TextFormatting.WHITE + "> " + TextFormatting.YELLOW + btnChangeKeyBinding.get(this).displayString + TextFormatting.WHITE + " <";
+		} else if (conflict) {
+			btnChangeKeyBinding.get(this).displayString = (modifierConflict ? TextFormatting.GOLD : TextFormatting.RED) + btnChangeKeyBinding.get(this).displayString;
+		}
+
+		btnChangeKeyBinding.get(this).drawButton(mc, mouseX, mouseY, partialTicks);
+	}
+
+}
